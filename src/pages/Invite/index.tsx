1594eb2915b79c89346315728555e0cc4c26a97e
Phoenix <phoenixdev1223@gmail.com>
2023-12-07 12:49:28 +0800

    accept invitation in case of loggedin user

--- src/pages/Invite/index.tsx
+++ src/pages/Invite/index.tsx
@@ -1,15 +1,38 @@
-import { useSearchParams, Navigate } from 'react-router-dom';
-import { AUTH_LOGIN_PATH, INVITATION_TOKEN } from 'src/constants';
+import React from "react";
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { AcceptMemberInvitation } from "src/libs/axios/api/project";
+import { AUTH_LOGIN_PATH, INVITATION_TOKEN, AUTH_ACCESS_TOKEN, AUTHENTICATED_ENTRY } from 'src/constants';
 
 const InviteHandler = () => {
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
-
     const token = searchParams.get('token');
-    if (token) {
+
+    if(token) {
         localStorage.setItem(INVITATION_TOKEN, token);
     }
 
-    return <Navigate to={AUTH_LOGIN_PATH} replace />
+    React.useEffect(() => {
+        if (localStorage.getItem(INVITATION_TOKEN)) {
+            if (localStorage.getItem(AUTH_ACCESS_TOKEN) === null) {
+                navigate(AUTH_LOGIN_PATH);
+            } else {
+                localStorage.removeItem(INVITATION_TOKEN);
+                AcceptInvitation();
+            }
+        }
+    }, [])
+
+    const AcceptInvitation = async () => {
+        const invitationToken = token
+        const res = await AcceptMemberInvitation(invitationToken as string)
+        if (res) {
+            console.log(res);
+        }
+        navigate(AUTHENTICATED_ENTRY);
+    }
+
+    return <></>
 }
 
 export default InviteHandler;
