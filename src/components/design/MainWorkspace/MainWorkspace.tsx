136e63f9b6a9f474052bdbbd3493297bc54082c8
sunnyalgo <coolman960505@gmail.com>
2024-01-09 17:17:48 +0900

    toolbar component drag

--- src/components/design/MainWorkspace/MainWorkspace.tsx
+++ src/components/design/MainWorkspace/MainWorkspace.tsx
@@ -1,6 +1,7 @@
-import { type FC, useState } from 'react'
+import { type FC, useState, LegacyRef, CSSProperties } from 'react'
 import { Button } from 'primereact/button';
 import { useDispatch, useSelector } from 'react-redux';
+import { useMouse } from 'primereact/hooks';
 import {
   SubScreen,
   Toolbar,
@@ -9,6 +10,11 @@ import {
   ButtonComponentDialog,
   ImageComponentDialog,
   AddScreenDialog,
+  ButtonComponent,
+  TextComponent,
+  LabelComponent,
+  ImageComponent,
+  Wrapper,
 } from 'components';
 import { IElement, IComponentType } from 'libs/types';
 import { initCurrentElement } from 'store/slices/viewTreeSlice';
@@ -19,7 +25,9 @@ import './MainWorkspace.scss';
 
 const MainWorkspace: FC<MainWorkspaceProps> = () => {
   const dispatch = useDispatch();
+  const { ref: newItemRef, x, y, reset } = useMouse();
   const { viewTrees, currentElement } = useSelector((state: RootState) => state.viewTree);
+  const { newToolSelected, ToolComponentID } = useSelector((state: RootState) => state.toolbar);
 
   const [isOpenAddScreenModal, setIsOpenAddScreenModal] = useState(false);
   const [mouseOut, setMouseOut] = useState(true);
@@ -47,8 +55,46 @@ const MainWorkspace: FC<MainWorkspaceProps> = () => {
     mouseOut && currentElement != null && dispatch(initCurrentElement(viewTrees));
   }
 
+  const getCurrentComponent = () => {
+    return (
+      <div
+        style={getDynamicComponentStyle()}
+      >
+        {getToolComponent()}
+      </div>
+    );
+  }
+
+  const getToolComponent = () => {
+    switch (ToolComponentID) {
+      case 0: return <ButtonComponent text="button"/>;
+      case 1: return <TextComponent />;
+      case 2: return <LabelComponent text="label" />;
+      case 3: return <ImageComponent />;
+      default: return <div/>;
+    }
+  }
+
+  const getDynamicComponentStyle = (): CSSProperties => {
+    const positionStyle: CSSProperties = {
+      position: 'absolute',
+      left: `${x}px`,
+      top: `${y}px`,
+      minHeight: 30,
+      minWidth: 100,
+      border: 1,
+      opacity: 0.3
+    }
+    return positionStyle;
+  };
+
   return (
-    <div className="workspace-body" onClick={selectionCheck}>
+    <div 
+      ref={newItemRef as LegacyRef<HTMLDivElement>}
+      className="workspace-body" 
+      onClick={selectionCheck}
+    >
+      {newToolSelected && getCurrentComponent()}
       <div className="screen-view">
         <Button icon="pi pi-plus" raised text onClick={AddNewScreenBtnClick} />
         <Toolbar items={toolBarItems} onClicked={toolSelected} />
