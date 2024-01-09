4c6d45d2cf4f70f303e87c60d0b1eb3da8359320
sunnyalgo <coolman960505@gmail.com>
2024-01-10 00:36:40 +0900

    change app-header

--- src/layouts/AppTopbar.tsx
+++ src/layouts/AppTopbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import { Avatar } from "primereact/avatar";
 import { Menu } from "primereact/menu";
 import { logout } from "store/slices/authSlice";
@@ -14,6 +15,9 @@ import "./AppTopbar.scss";
 
 const AppTopbar = () => {
   const { isLogged, currentUser } = useSelector((state: RootState) => state.auth);
+  const [ activeTab, setActiveTab ] = React.useState(0);
+  const location = useLocation();
+  const { projectId } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -60,11 +64,58 @@ const AppTopbar = () => {
       }
   }, [isLogged])
 
+  React.useEffect(() => {
+    if (projectId) {
+      switch (location.pathname) {
+        case `/app/project/${projectId}/design-workspace`:
+          setActiveTab(0)
+          break;
+        case `/app/project/${projectId}/backend-workspace`:
+          setActiveTab(1)
+          break;
+        case `/app/project/${projectId}/architecture-workspace`:
+          setActiveTab(2)
+          break;
+        default:
+          break;
+      }
+    }
+  }, [location, projectId])
+
+  const NavigateWorkspace = (index: number) => {
+    setActiveTab(index);
+    switch (index) {
+      case 0:
+        navigate(`/app/project/${projectId}/design-workspace`);
+        break;
+      case 1:
+        navigate(`/app/project/${projectId}/backend-workspace`);
+        break;
+      case 2:
+        navigate(`/app/project/${projectId}/architecture-workspace`);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="app-header">
       <div className="logo" onClick={ ()=>navigate('/app') }>
-        <h1>SINGULARITY</h1>
+        <h2>SINGULARITY</h2>
       </div>
+      {location.pathname.indexOf('preview') === -1 &&
+          <div className='tab-board'>
+            <div className="navigate-board">
+              <Button className={`nav-button${activeTab == 0 ? '-active' : ''}`} onClick={()=>{NavigateWorkspace(0)}} > DESIGN </Button>
+              <Button className={`nav-button${activeTab == 1 ? '-active' : ''}`} onClick={()=>{NavigateWorkspace(1)}}> BACK-END </Button>
+              <Button className={`nav-button`} disabled> ARCHITECTURE </Button>
+            </div>
+            <Button className="nav-button-preivew" size="small" onClick={() => navigate(`/app/project/${projectId}/preview`)}>
+              <span>PREVIEW</span>
+            </Button>
+          </div>
+        }
       <div className="profile">
         <Avatar
           shape="circle"
--- src/pages/App/Project/index.tsx
+++ src/pages/App/Project/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { TabView, TabPanel } from 'primereact/tabview';
-import { Button } from 'primereact/button';
-import { useNavigate, useParams, Navigate, Routes, Route, useLocation } from 'react-router-dom';
+import { useParams, Navigate, Routes, Route } from 'react-router-dom';
 import { getProjectById, setOpenAtById } from 'libs/axios/api/project';
 import { setProject, setStructure } from 'store/slices/projectSlice';
 import { InviteMembersDialog } from 'components/dialog/InviteMembersDialog';
@@ -30,35 +28,16 @@ const WorkspaceContent = () => {
 
 const Project = () => {
     const { projectId } = useParams();
-    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const location = useLocation();
     const [isOpenInviteModal, setIsOpenInviteModal] = React.useState(false);
     const [isOpenInvitationModal, setIsOpenInvitationModal] = React.useState(false);
     const [members, setMembers] = React.useState<IMember[]>([]);
-    const [activeTab, setActiveTab] = React.useState(0);
     const { project } = useSelector((state: RootState) => state.project);
 
     const GetProjectById = async (projectId: number) => {
         const res = await getProjectById(projectId);
         if (res) {
             dispatch(setProject(res as IProject));
-        }
-    }
-
-    const NavigateWorkspace = (index: number) => {
-        switch (index) {
-            case 0:
-                navigate(`/app/project/${projectId}/design-workspace`);
-                break;
-            case 1:
-                navigate(`/app/project/${projectId}/backend-workspace`);
-                break;
-            case 2:
-                navigate(`/app/project/${projectId}/architecture-workspace`);
-                break;
-            default:
-                break;
         }
     }
 
@@ -78,50 +57,8 @@ const Project = () => {
         }
     }, [project, projectId]);
 
-    React.useEffect(() => {
-        if (projectId) {
-            switch (location.pathname) {
-                case `/app/project/${projectId}/design-workspace`:
-                    setActiveTab(0)
-                    break;
-                case `/app/project/${projectId}/backend-workspace`:
-                    setActiveTab(1)
-                    break;
-                case `/app/project/${projectId}/architecture-workspace`:
-                    setActiveTab(2)
-                    break;
-                default:
-                    break;
-            }
-        }
-    }, [location, projectId])
-
     return (
         <div className="project-board">
-            {location.pathname.indexOf('preview') === -1 &&
-                <div className='tab-board'>
-                    <TabView 
-                        activeIndex={activeTab} 
-                        onTabChange={(e) => NavigateWorkspace(e.index)}
-                    >
-                        <TabPanel header="DESIGN" />
-                        <TabPanel header="BACKEND" />
-                        <TabPanel header="ARCHITECTURE" disabled/>
-                    </TabView>
-                    <Button
-                        style={{
-                            position: 'absolute',
-                            top: 6,
-                            right: 5
-                        }}
-                        raised
-                        size="small"
-                        onClick={() => navigate(`/app/project/${projectId}/preview`)}
-                    >
-                        <span>PREVIEW</span>
-                    </Button>
-                </div>
-            }
             <WorkspaceContent />
             <InviteMembersDialog
                 isOpenInviteModal={isOpenInviteModal}
